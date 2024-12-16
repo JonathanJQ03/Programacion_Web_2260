6a8826024c919ccd59ed52cbe78db7757bc536d8
Jonathan Javier Jaguaco Quituisaca <116780208+JonathanJQ03@users.noreply.github.com>
2024-12-16 17:47:12 +0000

    datosToposDAtops

--- Parcial 2/Ejercicio 2/first.js
+++ Parcial 2/Ejercicio 2/first.js
@@ -80,3 +80,96 @@ let a1nombresCompletos = "";
 let num1 = 45;
 let num2 = 78;
 console.log;
+
+//Precedencia de operadores
+//1. Parentesis
+//2. Exponente
+//3. Multiplicacion y division
+//4. Suma y resta
+//Se ejecutan de izquierda a derecha O DE DERECHA A IZQUIERDA segun sea el caso
+let pre1 = 2,
+  pre2 = 3,
+  pre3 = 4,
+  pre4 = 5;
+
+let pre5 = (pre1 + pre2) * pre3;
+console.log(pre5);
+
+let pre6 = (pre1 + pre2) * pre3;
+console.log(pre6);
+
+//Operadores de asignacion
+//asignacion =
+//Suma +
+//Resta -
+//Multiplicacion *
+//Division /
+//Modulo %
+//Exponente **
+
+let operador1 = 3,
+  operador2 = 2;
+operador1 += operador2;
+console.log(operador1);
+
+//Operadores de comparacion
+//Igual == :  se utiliza para comparar si dos vartiables son iguales en valor no es estricto
+//Distinto !=
+//Menor que <
+//Mayor que >
+//Menor o igual <=
+//Mayor o igual >=
+// Triple igual === :  se usa para comparar si dos vartiables son iguales en valor y del mismo tipo de  dato si es estricto
+let operador3 = 3,
+  operador4 = 3;
+let operador10 = operador3 == operador4;
+console.log(operador10);
+
+let operador11 = operador3 === operador4;
+console.log(operador11);
+
+//Determinar si un numero es par o impar
+let numeroD = 5;
+if (numeroD % 2 == 0) {
+  console.log("Es par");
+} else {
+  console.log("Es impar");
+}
+//Determinar si una persona es mayor de edad
+let numeroEdad = 12;
+if (numeroEdad > 18) {
+  console.log("Es mayor de edad");
+} else {
+  console.log("Es menor de edad");
+}
+//De un conjunto de valores de 10 valores , determinar cuandos son pares y cuantos son impares
+let conjunto = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let pares = 0;
+let impares = 0;
+for (let i = 0; i < conjunto.length; i++) {
+  if (conjunto[i] % 2 == 0) {
+    pares++;
+  } else {
+    impares++;
+  }
+}
+console.log(pares);
+console.log(impares);
+//Operadores logicos
+//AND &&
+//OR ||
+//NOT !
+
+let s = 12;
+let valMin = 12,
+  valMax = 20;
+if (s >= valMin && s <= valMax) {
+  console.log("Esta dentro del rango");
+} else {
+  console.log("No esta dentro del rango");
+}
+
+//operadores ternarios
+let edad = 10;
+let mensaje = edad >= 18 ? "Es mayor de edad" : "Es menor de edad";
+console.log(mensaje);
