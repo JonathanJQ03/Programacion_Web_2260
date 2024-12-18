c62cfbf1443f7718e81a6bfc3f7ac1e52aff831e
Jonathan Javier Jaguaco Quituisaca <116780208+JonathanJQ03@users.noreply.github.com>
2024-12-18 11:10:34 -0500

    SentenciasControl

---
+++ Parcial 2/SentenciasControl.js
@@ -0,0 +1,11 @@
+//Sentencias de control
+//if - else
+
+let numero = 10
+if(numero == 1){
+    console.log("El numero es 1");
+}else if(numero ==2){
+    console.log("El numero es 2");
+}else{
+    console.log("El numero no es 1 ni 2, es 10");
+}
